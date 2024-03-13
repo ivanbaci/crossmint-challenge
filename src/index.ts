23113fc465282ed526e4f4e84b8c12ec411354e3
Ivan Bacigaluppo <ivanbac1896@gmail.com>
2024-03-13 13:51:38 -0300

    feat: :loud_sound: Add some logs on main

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import { AstralObjectsFactory } from './factories/astralObjectsFactory';
 
 const main = async () => {
   const apiClient = new CrossmintApiClient();
+  console.log('Fetching goal map...');
   const goalMap = await apiClient.fetchGoalMap();
   const cells = new MapParser().parseMap(goalMap);
   console.log('Parsed map:', JSON.stringify(cells));
@@ -12,6 +13,11 @@ const main = async () => {
   console.log('Creating astral objects...');
   const astralObjectsFactory = new AstralObjectsFactory(apiClient);
   await astralObjectsFactory.createAstralObjects(cells);
+  console.log('Astral objects created successfully!');
+
+  console.log(
+    `Look at the map at https://challenge.crossmint.io/map with candidateId ${process.env.CANDIDATE_ID} to see the result!`
+  );
 };
 
 main();
