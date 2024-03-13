319607f8ea5c187d992b9200ef326e5a3590cc1c
Ivan Bacigaluppo <ivanbac1896@gmail.com>
2024-03-13 03:17:49 -0300

    feat: :sparkles: Call objects creation on main

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,17 @@
 import 'dotenv/config';
 import { CrossmintApiClient } from './crossmintApiClient';
 import { MapParser } from './mapParser';
+import { AstralObjectsFactory } from './astralObjectsFactory';
 
 const main = async () => {
   const apiClient = new CrossmintApiClient();
   const goalMap = await apiClient.fetchGoalMap();
-  const parsedMap = new MapParser().parseMap(goalMap);
-  console.log('Parsed map:', JSON.stringify(parsedMap));
+  const cells = new MapParser().parseMap(goalMap);
+  console.log('Parsed map:', JSON.stringify(cells));
+
+  console.log('Creating astral objects...');
+  const astralObjectsFactory = new AstralObjectsFactory(apiClient);
+  await astralObjectsFactory.createAstralsObjects(cells);
 };
 
 main();
