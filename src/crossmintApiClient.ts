ee9f48a452064f2ae1b3a71d78be162ca37b3e71
Ivan Bacigaluppo <ivanbac1896@gmail.com>
2024-03-12 22:08:43 -0300

    feat: :sparkles: New method on api client to create polyanets

--- src/crossmintApiClient.ts
+++ src/crossmintApiClient.ts
@@ -3,12 +3,23 @@ import { ApiCaller } from './apiCaller';
 
 export class CrossmintApiClient {
   private readonly baseUrl: string = `${process.env.BASE_API_URL}`;
+  private readonly candidateId: string = `${process.env.CANDIDATE_ID}`;
   private apiCaller: ApiCaller = new ApiCaller();
 
   public async fetchGoalMap(): Promise<string[][]> {
     const response: AxiosResponse = await this.apiCaller.callWithRetries(() =>
-      axios.get(`${this.baseUrl}/map/${process.env.CANDIDATE_ID}/goal`)
+      axios.get(`${this.baseUrl}/map/${this.candidateId}/goal`)
     );
     return response.data.goal;
+  }
+
+  public async createPolyanet(pos: { x: number; y: number }): Promise<void> {
+    await this.apiCaller.callWithRetries(() =>
+      axios.post(`${this.baseUrl}/polyanets`, {
+        column: pos.x,
+        row: pos.y,
+        candidateId: this.candidateId
+      })
+    );
   }
 }
