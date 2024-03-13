0f7d5f6dc3982899e225943bf38f3a5d1d6f359b
Ivan Bacigaluppo <ivanbac1896@gmail.com>
2024-03-13 05:02:12 -0300

    test: :white_check_mark: Add api caller tests

---
+++ tests/apiCaller.test.ts
@@ -0,0 +1,42 @@
+import axios from 'axios';
+import AxiosMockAdapter from 'axios-mock-adapter';
+import { ApiCaller } from '../src/apiCaller';
+
+describe('ApiCaller', () => {
+  const mockAxios = new AxiosMockAdapter(axios);
+  const caller = new ApiCaller();
+
+  afterEach(() => {
+    mockAxios.reset();
+  });
+
+  it('should successfully return data on first try', async () => {
+    const mockData = 'response';
+    mockAxios.onGet('url').reply(200, { mockData });
+
+    const result = await caller.callWithRetries(() => axios.get('url'));
+    expect(result.status).toBe(200);
+    expect(result.data).toMatchObject({ mockData });
+    expect(mockAxios.history.get.length).toBe(1);
+  });
+
+  it('should retry on failure and succeed on second try', async () => {
+    const mockData = 'response';
+
+    mockAxios.onGet('url').replyOnce(503);
+    mockAxios.onGet('url').replyOnce(200, mockData);
+
+    const result = await caller.callWithRetries(() => axios.get('url'));
+    expect(result.data).toBe(mockData);
+    expect(mockAxios.history.get.length).toBe(2);
+  });
+
+  it('should throw error after all retries failed', async () => {
+    mockAxios.onGet('url').reply(503);
+
+    await expect(
+      caller.callWithRetries(() => axios.get('url'))
+    ).rejects.toThrow('Call failed after all retries');
+    expect(mockAxios.history.get.length).toBe(4);
+  });
+});
